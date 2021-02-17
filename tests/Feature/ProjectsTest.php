e3be8a155bb859aef8e0939d5af81552c53395b9
Ryan Miller <ryanmillergm@gamil.com>
2021-02-17 03:54:30 -0700

    Adds Projects Tests

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -8,15 +8,25 @@ use Tests\TestCase;
 
 class ProjectsTest extends TestCase
 {
+    use WithFaker, RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_a_user_can_create_a_project()
     {
-        $response = $this->get('/');
+        $this->withoutExceptionHandling();
 
-        $response->assertStatus(200);
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph
+        ];
+
+        $this->post('/projects', $attributes);
+
+        $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
     }
 }
