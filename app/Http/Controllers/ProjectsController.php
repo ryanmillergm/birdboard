c643f7056cfcdb46bf50e467159b6f6a488375f2
Ryan Miller <ryanmillergm@gamil.com>
2021-02-27 23:27:24 -0700

    Updates store function and adds show

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -16,8 +16,18 @@ class ProjectsController extends Controller
 
     public function store()
     {
-        Project::create(request(['title', 'description']));
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        Project::create($attributes);
 
         return redirect('/projects');
+    }
+
+    public function show(Project $project)
+    {
+        return view('projects.show', compact('project'));
     }
 }
