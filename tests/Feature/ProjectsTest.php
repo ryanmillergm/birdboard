d28f45581daa06107243d6ad283b62b6578dd368
Ryan Miller <ryanmillergm@gamil.com>
2021-02-27 23:26:37 -0700

    Adds project validation tests

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Project;
 
 class ProjectsTest extends TestCase
 {
@@ -28,5 +29,34 @@ class ProjectsTest extends TestCase
         $this->assertDatabaseHas('projects', $attributes);
 
         $this->get('/projects')->assertSee($attributes['title']);
+    }
+
+
+    public function test_a_user_can_view_a_project()
+    {
+        $this->withoutExceptionHandling();
+
+        $project = Project::factory()->create();
+
+        $this->get($project->path())
+            ->assertSee($project->title)
+            ->assertSee($project->description);
+    }
+
+
+    // Validation Test
+    public function test_a_project_requires_a_title()
+    {
+        $attributes = Project::factory()->raw(['title' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+
+    public function test_a_project_requires_a_description()
+    {
+        $attributes = Project::factory()->raw(['description' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
 }
