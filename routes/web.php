6e261ca2addfbdc5ecc4f2ada988ec2b02668ea5
Ryan Miller <ryanmillergm@gamil.com>
2021-06-20 01:32:50 -0600

    Adds livewire and jetstream

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::resource('projects', ProjectsController::class)->only([
     'index', 'store', 'show'
 ])->middleware('auth');
 
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
