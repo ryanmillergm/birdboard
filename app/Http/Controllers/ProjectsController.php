50f83e4a79b1b704adb3025b7a5d7107fe47801d
Ryan Miller <ryanmillergm@gmail.com>
2022-01-01 16:45:36 -0700

    Creates project by current user

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -22,7 +22,9 @@ class ProjectsController extends Controller
             'description' => 'required',
         ]);
 
-        $attributes['owner_id'] = auth()->id();
+        // $attributes['owner_id'] = auth()->id();
+
+        auth()->user()->projects()->create($attributes);
 
         Project::create($attributes);
 
