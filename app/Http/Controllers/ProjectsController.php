9e8f85dad3a8114a883b4f21a5a9bf11c5e330e0
Ryan Miller <ryanmillergm@gmail.com>
2022-01-01 17:29:05 -0700

    Adds get only auth user projects for project index

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -9,14 +9,13 @@ class ProjectsController extends Controller
 {
     public function index()
     {
-        $projects = Project::all();
+        $projects = auth()->user()->projects;
 
         return view('projects.index', compact('projects'));
     }
 
     public function store()
     {
-        // dd('here we are');
         $attributes = request()->validate([
             'title' => 'required',
             'description' => 'required',
@@ -29,6 +28,9 @@ class ProjectsController extends Controller
 
     public function show(Project $project)
     {
+        // if (auth()->id() !== $project->owner_id) {
+        //     abort(403);
+        // }
         return view('projects.show', compact('project'));
     }
 }
