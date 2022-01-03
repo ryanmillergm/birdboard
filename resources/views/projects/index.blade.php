7fcc4539dd2479de45e9f3f2a24bcc4054df6870
Ryan Miller <ryanmillergm@gmail.com>
2022-01-03 00:37:14 -0700

    Updates styling to make cards for each project

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,20 +2,35 @@
 
 @section('content')
 
-    <div class="flex items-center mb-3">
+    {{-- This is an edited copy from the navbar to copy the flow--}}
+    {{-- <div class="flex justify-between h-16">
+        <div class="flex">
+            <!-- Logo -->
+            <h1 class="text-4xl text-black-700 font-semibold mr-auto">BirdBoard</h1>
+        </div>
+
+
+        <div class="flex items-center px-4">
+            <a href="{{ url('/projects/create')}}">New Project</a>
+        </div>
+    </div> --}}
+
+    <div class="flex items-center mb-3 px-4">
         <h1 class="text-4xl text-black-700 font-semibold mr-auto">BirdBoard</h1>
 
         <a href="{{ url('/projects/create')}}">New Project</a>
     </div>
 
-    <ul>
+    <div class="flex">
         @forelse ($projects as $project)
-            <li>
-                <a href="{{ $project->path() }}">{{ $project->title }}</a>
-            </li>
-        @empty
-            <li>No projects yet.</li>
-        @endforelse
-    </ul>
+            <div class="bg-white mr-4 p-5 rounded shadow-custom w-1/3" style="height: 200px;">
+                <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
 
+                <div class="text-grey">{{ Str::limit($project->description, 100) }}</div>
+            </div>
+        @empty
+            <div>No projects yet.</div>
+        @endforelse
+    </div>
 @endsection
+
